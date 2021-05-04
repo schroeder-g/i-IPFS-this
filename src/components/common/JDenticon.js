b56b266221d3b13f314c437ec72cf9d45d1e0fa2
Alex <schroedergoncalves@gmail.com>
2021-05-04 13:48:21 -0400

    removed failing dependencies (flippin' font awesome) and more unused components

--- src/components/common/JDenticon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const jdenticon = require("jdenticon");
-
-export default function JDenticon({value, size, className}) {
-    const svgIcon = jdenticon.toSvg(value, size);
-    return (
-        <div className={className} dangerouslySetInnerHTML={{__html: svgIcon}} />
-    )
-}
